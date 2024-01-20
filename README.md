49393998f070377a767a2e2f661408537f8cb55a
Aleksander Belomesytasev <lxwhlxwh@gmail.com>
2024-01-20 16:20:50 +0300

    Updated main logic to be more universal. Changed UI and edited README.md

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ISP check v1.0
 
 This application gets your external IP address with redundancy from two sources:
-1) http://myexternalip.com/
-2) https://api.ipify.org/
+
+1. http://myexternalip.com/
+2. https://api.ipify.org/
 
 Data from http://myexternalip.com/ gets twice, in RAW and in JSON type.
 
 Then data compares with each other and displays the actual data in a basic user interface.
 
-Script refresh in 5 seconds.
+Script refresh in 1 seconds.
--- script.js
+++ script.js
@@ -1,55 +1,35 @@
-let firstResultIP = "0";
-let secondResultIP = "0";
-let thirdResultIP = "0";
+let firstResultIP = "Loading...";
+let secondResultIP = "Loading...";
 let result = "";
 
-const ipOne = "http://myexternalip.com/raw";
-ipTwo = "https://api.ipify.org/?format=json";
-ipThree = "http://myexternalip.com/json";
+const ipfiData = "https://api.ipify.org/?format=json";
+const myExternalIPData = "http://myexternalip.com/json";
 
+// Fetching data from ipfi.org and myexternalip.com
 function checkISP() {
-  fetch(ipOne)
-    .then((resp) => resp.text())
-    .then((data) => console.log(data))
+  fetch(ipfiData)
+    .then((resp) => resp.json())
+    .then((data) => {
+      firstResultIP = data.ip;
+    })
     .catch((error) => {
-      "error in getting ip", error;
+      "Error: No data from ipify.org", error;
     });
 
-  fetch(ipTwo)
+  fetch(myExternalIPData)
     .then((resp) => resp.json())
     .then((data) => {
       secondResultIP = data.ip;
-      console.log(secondResultIP);
     })
     .catch((error) => {
-      "error in getting ip", error;
+      "Error: No data from Myexternalip.com", error;
     });
 
-  fetch(ipThree)
-    .then((resp) => resp.json())
-    .then((data) => {
-      thirdResultIP = data.ip;
-      console.log(thirdResultIP);
-    })
-    .catch((error) => {
-      "error in getting ip", error;
-    });
-
-  if (firstResultIP || secondResultIP || thirdResultIP === "109.124.78.178") {
-    result = "109.124.78.178 (Megafon)";
-  } else if (
-    firstResultIP ||
-    secondResultIP ||
-    thirdResultIP === "84.52.83.180 (WestCall)"
-  ) {
-    result = "84.52.83.180";
-  } else {
-    result = "Error, there is no connection to all 3 servers!";
-  }
-
-  document.getElementById("RESULTIP").innerText = result;
+  // Diplay data in UI
+  document.getElementById("RESULTIP").innerText = firstResultIP;
+  document.getElementById("RESULTIPTHIRD").innerText = secondResultIP;
   document.getElementById("DATE").innerText = new Date().toLocaleTimeString();
 }
-checkISP();
+checkISP(); // Calls function for first time.
 
-setInterval(checkISP, 5000);
+setInterval(checkISP, 1000); // Repeats function checkISP every second
